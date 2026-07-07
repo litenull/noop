f1f8fd8d217610aeed37511e7318e028678c23e0
ryanbr <mp3geek@gmail.com>
2026-07-07 19:57:15 +0000

    Release 9.0.4: bump version (patch) + build numbers + What's New

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.noop.whoop"
         minSdk = 26
         targetSdk = 34
-        versionCode = 268
-        versionName = "9.0.3"
+        versionCode = 269
+        versionName = "9.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
